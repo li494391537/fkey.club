01624a421b8fd0f7e365aed3294eb05a4c2860b6
黎睿 <lirui15@yonyou.com>
2019-05-24 17:23:03 +0800

    test

--- members.js
+++ members.js
@@ -1,8 +1,12 @@
 const members = [
-  { code: '001', name: '第1个中奖的b' }
-  , { code: '002', name: '第2个中奖的b' }
-  , { code: '003', name: '第3个中奖的b' }
-  , { code: '004', name: '第4个中奖的b' }
-  , { code: '005', name: '第5个中奖的b' }
+  { code: '001', name: '第1个b' }
+  , { code: '002', name: '第2个b' }
+  , { code: '003', name: '第3个b' }
+  , { code: '004', name: '第4个b' }
+  , { code: '005', name: '第5个b' }
+  , { code: '006', name: '第6个b' }
+  , { code: '007', name: '第7个b' }
+  , { code: '008', name: '第8个b' }
+  , { code: '009', name: '第9个b' }
 ]
 export { members };
