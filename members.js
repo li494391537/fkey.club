5814585e8e64a0809c29d3a950800f7a83525081
Kaboom Mirai <lirui494391537@outlook.com>
2019-05-24 14:19:38 +0800

    test

---
+++ members.js
@@ -0,0 +1,10 @@
+const members = [
+  { code: '001', name: 'test001' }
+  , { code: '002', name: 'test002' }
+  , { code: '003', name: 'test003' }
+  , { code: '004', name: 'test004' }
+  , { code: '005', name: 'test005' }
+  , { code: '006', name: 'test006' }
+  , { code: '007', name: 'test007' }
+]
+export { members };
